67e8ff9f58913737256744f59a8f8a86ccb33513
Tushar Verma <123294028+gitboytushar@users.noreply.github.com>
2024-11-20 18:39:40 +0530

    Added New Loader that works on Production env and Refinements

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,44 @@
-import { StrictMode } from 'react'
+import React, { StrictMode, useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Loading from './components/Loading.jsx'
-import DelayedRender from './components/DelayedRender.jsx'
+import { RiSparkling2Fill } from '@remixicon/react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <DelayedRender delay={1500} fallback={<Loading />}>
-      <App />
-    </DelayedRender>
-  </StrictMode>
+// custom loader
+const Loading = () => (
+  <div className='flex items-center justify-center w-full h-full fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+    <p className='flex items-center justify-center gap-2 md:gap-4 text-center text-sm md:text-2xl'>
+      <RiSparkling2Fill className='w-4 md:w-5 lg:w-8 h-4 md:h-5 lg:h-8 motion-preset-blink motion-duration-1000' />
+      <span className='motion-preset-fade motion-duration-2000 bg-gradient-to-r from-yellow-200 via-orange-500 to-violet-500 bg-clip-text text-transparent'>
+        Welcome to Tushar's Portfolio
+      </span>
+      <RiSparkling2Fill className='w-4 md:w-5 lg:w-8 h-4 md:h-5 lg:h-8 motion-preset-blink motion-duration-1000' />
+    </p>
+  </div>
 )
+
+// main call
+const Root = () => {
+  const [pageLoaded, setPageLoaded] = useState(false)
+  const [animationComplete, setAnimationComplete] = useState(false)
+
+  useEffect(() => {
+    const handleLoad = () => setPageLoaded(true)
+    window.addEventListener('load', handleLoad)
+
+    const timeout = setTimeout(() => setAnimationComplete(true), 2500)
+
+    return () => {
+      window.removeEventListener('load', handleLoad)
+      clearTimeout(timeout)
+    }
+  }, [])
+
+  return (
+    <StrictMode>
+      {!(pageLoaded && animationComplete) ? <Loading /> : <App />}
+    </StrictMode>
+  )
+}
+
+createRoot(document.getElementById('root')).render(<Root />)
